17fd90d31794d9433642514193a9c18380e67d0d
kelvinsekx <utdkelvin@gmail.com>
2020-04-09 17:44:06 -1200

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function IndexPage() {
               className="img-wrapper"
               style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}
             >
-              <img src={`../../static/img.jpg`} alt="shiny stuff" />
+              <img src={`../../static/img.jpg`} alt="Kelvin Ukuejubola Images" />
             </div>
           </div>
         </div>
