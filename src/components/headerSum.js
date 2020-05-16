2fa6aaf820b416d0c06086ce8809ee8525b225bd
kelvinsekx <utdkelvin@gmail.com>
2020-05-15 13:27:28 -1200

    changes

--- src/components/headerSum.js
+++ src/components/headerSum.js
@@ -6,7 +6,8 @@ export default function headerSum() {
           <div className="detailsContainer">
             <h2>Ukuejubola Kelvin</h2>
             <p className="details grey">
-              Software Engineer and Web Expert inside Life
+              Software Engineer and Web Expert
+              <div>JavaScript | Web Development</div>
             </p>
           </div>  
         </>
