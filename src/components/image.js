eab250f9a1e6417b333cbec93c8ce85cc857936b
kelvinsekx <utdkelvin@gmail.com>
2020-05-25 08:16:48 -1200

    wawu

--- src/components/image.js
+++ src/components/image.js
@@ -16,57 +16,98 @@ import Img from "gatsby-image"
 //   `)
 
 //   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+
+// the formershits
+// graphql`
+//       query {
+//         allFile(
+//           filter: {
+//             dir: {
+//               eq: "C:/nodePractice/gatsby/deployer/gatsbyblog/src/images/slides"
+//             }
+//           }
+//         ) {
+//           edges {
+//             node {
+//               id
+//               name
+//               childImageSharp {
+//                 fluid(maxWidth: 190, maxHeight: 190) {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `
 // }
 
 function Image() {
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(1)
 
-  const { allFile } = useStaticQuery(
-    graphql`
+  const data  = useStaticQuery(
+      graphql` 
       query {
-        allFile(
-          filter: {
-            dir: {
-              eq: "C:/nodePractice/gatsby/deployer/gatsbyblog/src/images/slides"
-            }
-          }
-        ) {
-          edges {
-            node {
-              id
-              name
-              childImageSharp {
-                fluid(maxWidth: 190, maxHeight: 190) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
+      image1:file(relativePath: { eq: "kelvin-hands.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 190, maxHeight: 190) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
-    `
+
+      image2:file(relativePath: { eq: "kelvin-ugly.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 190, maxHeight: 190) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      image3:file(relativePath: { eq: "kelvinsmilling.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 190, maxHeight: 190) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      image4:file(relativePath: { eq: "yeye.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 190, maxHeight: 190) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `
+    
   )
 
-  const length = allFile.edges.length - 1
+  //const length = allFile.edges.length - 1
 
   //setInterval(changeIndex, 1000)
-  React.useEffect(() => {
+ React.useEffect(() => {
     const interval = setInterval(
       _ => {
       // let new_index = Math.floor(Math.random() * length)
-      index === length ? setIndex(0) :
+      index === 2 ? setIndex(0) :
       setIndex(index+1)
     }, 4000)
     return _ => clearInterval(interval)
   })
+  
+  const arrayIt = [
+     {image: data.image2.childImageSharp.fluid},
+     {image: data.image3.childImageSharp.fluid},
+     {image: data.image4.childImageSharp.fluid},
+  ]
+  //   window.requestAnimationFrame(changeIndex);
 
-  const { node } = allFile.edges[index]
+  let image = arrayIt[index].image
 
-  // window.requestAnimationFrame(changeIndex);
-
-  return (
-    <Img fluid={node.childImageSharp.fluid} key={node.id} alt={node.name} />
-  )
+  return <Img fluid={image } />
 }
 
 export default Image
