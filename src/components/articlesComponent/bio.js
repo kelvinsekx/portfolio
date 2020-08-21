0308d40f81fd684dfd28af538ec24ef9229758c6
kelvinsekx <utdkelvin@gmail.com>
2020-08-20 19:23:57 -1200

    asynchronous node

--- src/components/articlesComponent/bio.js
+++ src/components/articlesComponent/bio.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default ({ data, description }) => (
   <div style={{fontSize:'2rem', 
-  textShadow: '5px 4px 6px black, 0 0 1em blue, 0 0 0.7em blue'}}>
+  textShadow: '3px 2px 3px black, 0 0 1em blue, 0 0 0.3em blue'}}>
     <h1>{data}</h1>
     <div>
       <p>{description}</p>
