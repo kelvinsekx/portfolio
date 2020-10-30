0e7d8c1ee0cf42f96cfc3d5dd67693cc636f2547
kelvinsekx <utdkelvin@gmail.com>
2020-10-30 18:29:40 +0100

    OMG

--- src/components/typing/typing.js
+++ src/components/typing/typing.js
@@ -1,32 +1,29 @@
 import React from "react"
-import Typed from 'typed.js';
-
+import LetterLoading from "letterloading";
 
 class Typing extends React.Component{
+    
     componentDidMount(){
         const options ={
-            strings: ["YOU, yes <em style='color: violet;font-size: 1.4em'>YOU<em>","my <b style='color: #345678'>MUM</b> ","<b style='color: #505060'>Terri Burns</b>, ","travel", "<em style='font-size: 0.7em'>anything related to rihanna</em>",
-            "<em style='color: violet'>JAYZ more</em>", "reading","SORRY, love <em style='color: yellow'>GOD</em>",
-            "ramen", "Ibadan", " two HAUSA girls", "seeing small jokes", "<b style='color:green'>not tweeting</b>", "BEING me", "wine ^(* ! *)^",
+            strings: ["YOU, yes YOU","MUM ","Terri Burns, ","travel", "anything related to rihanna",
+            "JAYZ more", "reading","SORRY, love GOD",
+            "ramen", "Ibadan", " two HAUSA girls", "seeing small jokes", "not tweeting", "BEING me", "wine ^(* ! *)^",
             "to-do lists", "public speaking", "lorde _//\\_", "nike shoes", "interior design", "dates", "the internet","the WEB",
-            "<em style='font-size: 0.7em'>Havard Business School</em>",
-            "<em style='font-size: 0.7em'>Wharton Business School</em>", " INSEAD", "chicago","chicago boothe", "side hustles", "doing vs talking", "my bed",
-            "Game of Thrones", "weekends", "restaurants", "FOOD :(", "black twitter", "<em style='font-size: 0.6em'>iphones,but never used one</em>", "crop tops", "writing",
+            "Havard Business School",
+            "Wharton Business School", " INSEAD", "chicago","chicago boothe", "side hustles", "doing vs talking", "my bed",
+            "Game of Thrones", "weekends", "restaurants", "FOOD :(", "black twitter", "iphones,but never used one", "crop tops", "writing",
             "looking funny", "and may be dump", "figure 8", "house plants", "mornings",
             "sunshine, ","indoor", " kids", "like the POOR", "the humble",
-            "<b style='color: yellow'>being loved</b>"],
-          typeSpeed: 70,
-          backSpeed: 50,
-          showCursor: true,
-          loop: false,
-          loopCount: false,
-          backDelay: 1000,
+            "being loved"],
+            loadspeed: 2,
+            randomizeEl: false,
+            char: "",
           };
-          this.typed = new Typed(this.el, options);
+          this.letterload = new LetterLoading(this.el, options);
     }
 
     componentWillUnmount(){
-        this.typed.destroy();
+        this.letterload.destroy();
     }
     render(){
         return(
