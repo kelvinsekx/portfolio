af92412da91ae92daf2a9544b2306b7bdd6bfd4c
kelvinsekx <utdkelvin@gmail.com>
2023-10-30 06:16:37 +0000

    resolving assets import

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// custom typefaces
-// import "typeface-montserrat"
-// import "typeface-merriweather"
-
-//bootstrap 
-import 'bootstrap/dist/css/bootstrap.css';
-
-import "prismjs/themes/prism.css"
