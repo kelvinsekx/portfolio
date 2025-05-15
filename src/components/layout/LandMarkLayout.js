79c02c69525aed3208e4e014415f28eb118a5af3
kelvinsekx <utdkelvin@gmail.com>
2025-05-15 12:40:29 +0100

    wip: center texts

--- src/components/layout/LandMarkLayout.js
+++ src/components/layout/LandMarkLayout.js
@@ -5,7 +5,7 @@ export const LandMarkLayout = ({ children }) => {
     <div>
       {children}
       <footer>
-        <p className="text-gray-500 px-2 text-center w-9/12 text-balance py-1 mt-5">
+        <p className="text-gray-500 px-2 text-center w-9/12 text-balance py-1 mt-5 flex justify-center items-center">
           This website is built by myself together with React, GraphQL, Gatsby
           build & Vercel. Cascaded with TailwindCSS & Styled Components.
         </p>
