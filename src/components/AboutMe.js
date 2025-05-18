51837a013817c0339b7d00cccdb3f6f4275efe9c
kelvinsekx <utdkelvin@gmail.com>
2025-05-18 14:59:31 +0100

    update long bio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -112,12 +112,12 @@ export function Long() {
   const r = randColor()
   return (
     <div className="space-y-3">
-      <p> Well, Thanks for wanting to read the details.</p>
+      <p>Thanks for taking the time to learn more about me!</p>
       <p>
-        My government name is Ukuejubola Kelvin Oritsetimeyin. I hail from Delta
-        State, Nigeria. But I have lived most part of my life in Ibadan. I am an
-        developer, product manager and economist. A graduate of Economics and
-        Education (B.sc[Ed]) from the{" "}
+        My government name is Ukuejubola Kelvin Oritsetimeyin. And while I
+        proudly hail from Delta State, Nigeria, Ibadan has been home for most of
+        my life. I wear a few hats: developer, product manager, and economist.
+        My academic foundation is in Economics and Education (B.sc[Ed]) from the{" "}
         <a
           style={{ color: colors[r], fontWeight: 500 }}
           href="https://www.ui.edu.ng"
@@ -127,46 +127,52 @@ export function Long() {
         .
       </p>
       <p>
-        In my 5+ years of experience, I have worked full-time with{" "}
+        Over the past 5 years and counting, I have gained valuable experiences
+        from working with{" "}
         <UnderlinedLink href={"https://www.liinae.com"}>Liinae</UnderlinedLink>,{" "}
         <UnderlinedLink href={"https://www.sproutly.africa"}>
           Sproutly
         </UnderlinedLink>
         , Wigxel Corp, Concrete Communication & OlaOla Poundo Yam. These are
-        excluding dozens of short-lived gigs. I am passionate about open source.
-        I have helped improve a handful of them like opensauced.pizza and
-        langcorrect.com.
+        excluding dozens of short-lived projects. I am a strong advocate for
+        open source. I have helped improve a handful of them like
+        opensauced.pizza and langcorrect.com.
       </p>
       <p>
-        Before my coding career, I was into oil and acrylic painting in my early
-        undergrad days (2014-2016). I began my programming in final year during
-        the ASSU strike (internal to UI). I would use my free time during this
-        strike to learn HTML, CSS, and PHP.
+        My creative sides emerged in my early undergrads years (2014-2016)
+        through oil and acrylic painting. My programming journey began just
+        after, in my final year, during an Academic Staff Union of Universities
+        (ASUU) strike. That unexpected free time became an opportunity to dive
+        into HTML, CSS, and PHP.
       </p>
       <p>
-        After some time learning PHP, I got introduced into javascript by{" "}
-        <a href="https://www.linkedin.com/in/oluwatayo-adegboye">Oluwatayo</a>.
-        I fell in love with the animations that comes with it. The rest they say
-        is history.
+        A pivotal moment came when{" "}
+        <a
+          href="https://www.linkedin.com/in/oluwatayo-adegboye"
+          className="underline"
+        >
+          Oluwatayo
+        </a>{" "}
+        introduced me to javascript . The dynamic animations instantly
+        captivated me, and as they say, the rest is history!
       </p>
       <p>
-        Because of my love for econometrics, I spend few time playing with
-        python, and how it could be used to enhance econometrics. To me,
-        econometrics is the future of economics and its application would mean
-        so much for the field.
+        My fascination with econometrics has also led me to explore Python and
+        its potential to enhance the field. I genuinely believe econometrics
+        holds the key to the future of economics and its practical applications.
       </p>
       <p>
-        You've just learnt a lot about my background. Let me show you a part of
-        me only people who knows so much about me see, I'm very jovial and no
-        dull moment.
+        So, there you have it, a deeper look into my background. Those who know
+        me well can attest: I'm quite jovial, and there's rarely a dull moment
+        around me!
       </p>
       <p>
-        Here is your reward 🤣. TAKE THIS JOKE:{" "}
+        Here's a little something for your time 🤣:{" "}
         <span className="text-black font-medium">
-          Do you know that tall children are not taller than their parents?
-          Well, if you don't know, now you know at least. If you still don't
-          believe, then you would have been taller than the tallest tree in your
-          home town
+          Did you know that tall children are not taller than their parents?
+          Well, if you didn't, now you do! If you still find that hard to
+          believe, you'd have to be taller than the tallest tree in your
+          hometown
         </span>
         . lol
       </p>
