8e60fba88588482883aed1086bcd80678f260a97
kelvinsekx <utdkelvin@gmail.com>
2025-05-26 07:24:48 +0100

    adjust spacing

--- src/components/ListRecentArticles.js
+++ src/components/ListRecentArticles.js
@@ -24,27 +24,29 @@ export const ListRecentArticles = () => {
     }
   `)
   return (
-    <div className="mt-12 border-t space-y-5">
-      <h2 className="text-lg font-semibold">My recent writings</h2>
-      <div className="grid grid-cols-1 gap-2">
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <Link to={node.fields.slug} className="md:w-8/12 group">
-            <div
-              key={node.id}
-              className="bg-[#f5f5f5]/20 group-hover:bg-gray-50 rounded-xl text-sm border px-4 py-3"
-            >
-              <h3 className="text-base font-medium group-hover:text-black text-[#444] capitalize">
-                {node.frontmatter.title}
-              </h3>
-              <p className="text-gray-600 text-xs">
-                Published on {node.frontmatter.date}
-              </p>
-              <p className="text-blue-500 group-hover:underline text-xs mt-1">
-                Read more &raquo;
-              </p>
-            </div>
-          </Link>
-        ))}
+    <div className="pt-20">
+      <div className="border-t space-y-5">
+        <h2 className="text-lg font-semibold">My recent writings</h2>
+        <div className="grid grid-cols-1 gap-2">
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <Link to={node.fields.slug} className="md:w-8/12 group">
+              <div
+                key={node.id}
+                className="bg-[#f5f5f5]/20 group-hover:bg-gray-50 rounded-xl text-sm border px-4 py-3"
+              >
+                <h3 className="text-base font-medium group-hover:text-black text-[#444] capitalize">
+                  {node.frontmatter.title}
+                </h3>
+                <p className="text-gray-600 text-xs">
+                  Published on {node.frontmatter.date}
+                </p>
+                <p className="text-blue-500 group-hover:underline text-xs mt-1">
+                  Read more &raquo;
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   )
