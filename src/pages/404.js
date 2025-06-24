6bfb23cd4924558af1a4f2ca186353fd5f18a39e
kelvinsekx <utdkelvin@gmail.com>
2025-06-24 07:32:26 +0100

    wip: resolving vercel build error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,18 @@
 import React from "react"
 
-import Layout from "../components/layouts/land-mark-layout"
+import { LandMarkLayout } from "../components/layouts/land-mark-layout"
 import SEO from "../components/seo"
 // <link rel="stylesheet" href="https://get.mavo.io/mavo.css">
 // <script src="https://get.mavo.io/mavo.js"></script>
 import PepperDem from "../components/pepperdem"
 
 const NotFoundPage = () => (
-  <Layout>
+  <LandMarkLayout>
     <PepperDem title={12} />
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </LandMarkLayout>
 )
 
 export default NotFoundPage
